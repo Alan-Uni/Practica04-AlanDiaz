b0a3ad4c7c770e5025201d443cd7c92a30dbaf70
Alan-Uni <144484127+Alan-Uni@users.noreply.github.com>
2023-09-26 12:29:26 -0600

    Version final

--- 3OLIDTS-AlanDiaz-04/_3OLIDTS_AlanDiaz_04.py
+++ 3OLIDTS-AlanDiaz-04/_3OLIDTS_AlanDiaz_04.py
@@ -1,5 +1,26 @@
 import tkinter as tk
 
+
+def borrar ():
+    entry1.delete(0,tk.END)
+    entry1.insert(0, "0.0")
+    entry2.delete(0,tk.END)
+    entry2.insert(0, "0.0")
+    
+def convertir_a_farenheit():
+    celsius=float(entry2.get())
+    farenheit=(celsius*9/5)+32
+    entry1.delete(0,tk.END)
+    entry1.insert(0,f"{farenheit:.2f}")
+    
+def convertir_a_celsius():
+    farenheit=float(entry1.get())
+    celsius=(farenheit-32)*5.0/9.0
+    entry2.delete(0,tk.END)
+    entry2.insert(0,f"{celsius:.2f}")
+
+
+
 ventana=tk.Tk()
 ventana.title("Conversor de temperatura")
 
@@ -9,7 +30,7 @@ label1.grid(row=0,column=0)
 entry1= tk.Entry(ventana)
 entry1.grid(row=0, column=1)
 
-button1= tk.Button(ventana, text="Convertir a Celsius") #,comand=convertir_a_celsius)
+button1= tk.Button(ventana, text="Convertir a Celsius",command=convertir_a_celsius)
 button1.grid(row=0, column=2)
 
 label2= tk.Label(ventana, text='Celsius')
@@ -18,10 +39,10 @@ label2.grid(row=1,column=0)
 entry2= tk.Entry(ventana)
 entry2.grid(row=1, column=1)
 
-button2= tk.Button(ventana, text="Convertir a Farenheit")#,comand=convertir_a_farenheit)
+button2= tk.Button(ventana, text="Convertir a Farenheit",command=convertir_a_farenheit)
 button2.grid(row=1, column=2)
 
-button3= tk.Button(ventana, text="Reestablecer")#,comand=borrar)
+button3= tk.Button(ventana, text="Reestablecer",command=borrar)
 button3.grid(row=2, column=1)
 
 ventana.geometry("520x480")
